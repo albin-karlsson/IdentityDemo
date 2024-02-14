0976c655db850fc65f755dc8a95a14f782ab2b83
albin-karlsson <55614148+albin-karlsson@users.noreply.github.com>
2024-02-14 15:23:55 +0100

    Initial commit

---
+++ Pages/Member/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityDemo.Pages.Member
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
